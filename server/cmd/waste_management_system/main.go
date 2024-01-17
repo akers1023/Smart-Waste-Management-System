9e0d0c1689ce4b640d0b8787eaa15dc0139f0c81
akers1023 <myytb20095511@gmail.com>
2024-01-17 16:36:46 +0700

    update main env

--- server/cmd/waste_management_system/main.go
+++ server/cmd/waste_management_system/main.go
@@ -3,18 +3,43 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"strconv"
+
+	"github.com/akers1023/Smart-Waste-Management-System/internal/connections"
+	"github.com/akers1023/Smart-Waste-Management-System/internal/models"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading env file")
+	}
 
+	port, err := strconv.Atoi(os.Getenv("DB_PORT"))
+	if err != nil {
+		log.Fatal("DB_PORT must be a valid integer")
+	}
+	sql := &connections.Sql{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     port,
+		Password: os.Getenv("DB_PASS"),
+		UserName: os.Getenv("DB_USER"),
+		SSLMode:  os.Getenv("DB_SSLMODE"),
+		DbName:   os.Getenv("DB_NAME"),
+	}
+
+	db, _ := sql.Connect()
+	defer sql.Close()
+
+	err = models.MigrateUser(db)
+	if err != nil {
+		log.Fatal("could not migrate db")
+	}
+	fmt.Println("ok con de")
 	app := fiber.New()
-
-	app.Get("/nguyencongmanh", func(c *fiber.Ctx) error {
-		return c.SendString("OK")
-	})
-
 	log.Fatal(app.Listen(":1234"))
 }
