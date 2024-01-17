f1f878597d809307c0934e33d6db4e394e3f1b01
akers1023 <myytb20095511@gmail.com>
2024-01-17 14:14:18 +0700

    update field user_model

--- server/internal/models/user_model.go
+++ server/internal/models/user_model.go
@@ -6,17 +6,25 @@ import (
 	"gorm.io/gorm"
 )
 
+// username va email bo sung xac thuc dang nhap sau
+// xem lai category nen dung string hay dung pointer string
 type User struct {
 	ID            string    `json:"id" gorm:"primaryKey"`
 	Full_name     string    `json:"full_name"`
-	First_name    *string   `json:"first_name"`
-	Middle_name   *string   `json:"middle_name"`
-	Last_name     *string   `json:"last_name"`
+	First_name    *string   `json:"first_name" validate:"required,min=2,max=100"`
+	Middle_name   *string   `json:"middle_name" validate:"required,min=2,max=100"`
+	Last_name     *string   `json:"last_name" validate:"required,min=2,max=100"`
+	Gender        *string   `json:"gender" validate:"required,eq=male|eq=female"`
 	Date_of_birth time.Time `json:"date_of_birth"`
-	Email         *string   `json:"email" validate:"email,required"`
-	Phone         *string   `json:"phone" validate:"phone,"`
+	Nationality   *string   `json:"nationality" validate:"required"`
+	CIN           *[12]byte `json:"cin" validate:"required,min=12"`
+	POO           *string   `json:"poo" validate:"required"`
+	POR           *string   `json:"por" validate:"required"`
+	Email         *string   `json:"email"`     // validate: email
+	User_name     *string   `json:"user_name"` // validate: user
+	Phone         *string   `json:"phone,omitempty" validate:"omitempty,phone"`
 	Password      *string   `json:"password" validate:"required,min=6"`
-	Total_points  *float64  `json:"total_points"`
+	Category      *string   `json:"category" validate:"required,eq=fulltime,eq=parttime"`
 	Token         *string   `json:"token"`
 	RefreshToken  *string   `json:"refresh_token"`
 	CreatedAt     time.Time `json:"created_at"`
